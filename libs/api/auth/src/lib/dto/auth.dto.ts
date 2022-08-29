3a6572ec282322afee9f1c7759df1e82c3d04bba
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-08-29 15:27:56 +0600

    feat: Improve validator by adding custom validation username

--- libs/api/auth/src/lib/dto/auth.dto.ts
+++ libs/api/auth/src/lib/dto/auth.dto.ts
@@ -4,9 +4,37 @@ import {
   IsString,
   MaxLength,
   MinLength,
+  ValidateBy,
+  ValidateByOptions,
+  ValidationOptions,
 } from 'class-validator';
 
-export class AuthSignupDto {
+const usernameValidator: [ValidateByOptions, ValidationOptions] = [
+  {
+    name: 'username',
+    validator: {
+      validate: ([first, ...rest]: string[]) =>
+        !/\d|[A-Z]/.test(first) &&
+        rest.every((letter) => /[a-z]|\d/.test(letter)),
+    },
+  },
+  {
+    message: 'username must be lowercase & start with alphabets',
+  },
+];
+
+class SininWithUsername {
+  @IsNotEmpty()
+  @IsEmail()
+  @ValidateBy(...usernameValidator)
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  password: string;
+}
+class SigninWithEmail {
   @IsNotEmpty()
   @IsEmail()
   email: string;
@@ -17,12 +45,20 @@ export class AuthSignupDto {
   password: string;
 }
 
-export class AuthSigninDto {
+export type AuthSigninDto = SigninWithEmail | SininWithUsername;
+
+export class AuthSignupDto {
   @IsNotEmpty()
   @IsString()
   @MinLength(3)
   @MaxLength(50)
   fullname: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @ValidateBy(...usernameValidator)
+  username: string;
 
   @IsNotEmpty()
   @IsEmail()
