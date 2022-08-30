8ccaefa5f0ef66bf4e73f824309e69daa098a6c9
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-08-30 10:43:01 +0600

    fix: imported the ConfigModule to PrismaModule

--- libs/api/prisma/src/lib/api-prisma.module.ts
+++ libs/api/prisma/src/lib/api-prisma.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ApiPrismaService } from './api-prisma.service';
 
 @Module({
+  imports: [ConfigModule],
   providers: [ApiPrismaService],
   exports: [ApiPrismaService],
 })
