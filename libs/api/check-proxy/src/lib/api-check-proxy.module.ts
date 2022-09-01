f732aa4276c767e66955df4070c9d50981101627
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-09-01 13:01:58 +0600

    feat: move the basic strategy to extra module

--- libs/api/check-proxy/src/lib/api-check-proxy.module.ts
+++ libs/api/check-proxy/src/lib/api-check-proxy.module.ts
@@ -1,11 +1,11 @@
+import { ApiSharedBasicStrategyModule } from '@api-shared/basic-strategy';
 import { ApiPrismaModule } from '@api/prisma';
 import { Module } from '@nestjs/common';
 import { CheckProxyController } from './check-proxy/check-proxy.controller';
 import { CheckProxyService } from './check-proxy/check-proxy.service';
-import { AuthModule } from './strategy/auth-strategy.module';
 
 @Module({
-  imports: [ApiPrismaModule, AuthModule],
+  imports: [ApiPrismaModule, ApiSharedBasicStrategyModule],
   controllers: [CheckProxyController],
   providers: [CheckProxyService],
   exports: [],
---
+++ prisma/migrations/20220901065331_rotate_proxy/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "ProxyList" ADD COLUMN     "rotatingId" INTEGER NOT NULL DEFAULT 0;
