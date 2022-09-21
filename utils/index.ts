aaa4eb4be50fe259a7dfcde9a759dd72bcec6d0a
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-09-21 16:14:06 +0600

    enhance: create an extra func for unique error & update exceptions

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,25 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+
+export const isUniqueError = (e: Error): HttpException | boolean => {
+  if (e instanceof Prisma.PrismaClientKnownRequestError) {
+    if (e.code === 'P2002') {
+      const uniqueProp: string = e.message
+        .match(/\(`(.+)`\)/g)!
+        .at(0)!
+        .replace(/[()`]/g, '');
+
+      // if username & email already registered
+      return new HttpException(
+        `${uniqueProp} already registered`,
+        HttpStatus.BAD_REQUEST
+      );
+    }
+  }
+
+  return false;
+};
+
 export const validateUsername = ([first, ...rest]: string): boolean =>
   !/\d|[A-Z]/.test(first) && rest.every((letter) => /[a-z]|\d/.test(letter));
