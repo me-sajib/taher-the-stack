98a4ce00c8b743e9fd68b0daa24b1a37fca09ed4
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-09-22 10:05:36 +0600

    feat(client): add user update thunk for update and validation

--- apps/client/pages/profile.tsx
+++ apps/client/pages/profile.tsx
@@ -2,25 +2,18 @@ import { Container } from '@mui/system';
 import { User } from '@prisma/client';
 import Page from 'components/Page';
 import DashboardLayout from 'layouts/dashboard';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { getProfile } from 'store/userSlice';
 // React
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import ChangePasswordForm from 'sections/profile/ChangePasswordForm';
 import UserUpdateFrom from 'sections/profile/UserUpdateForm';
 // redux
 import { FormControlLabel, Switch } from '@mui/material';
-import { AppThunkDispatch } from 'store';
-import { fetchUserProfile } from 'store/thunks';
 
 export default function Profile() {
   const profile: User = useSelector(getProfile);
-  const asyncDispatch = useDispatch<AppThunkDispatch>();
   const [updatePassSwitch, setPasswordStatus] = useState(false);
-
-  useEffect(() => {
-    asyncDispatch(fetchUserProfile());
-  }, [updatePassSwitch]);
 
   const togglePassStatus = () => setPasswordStatus((prev) => !prev);
 
