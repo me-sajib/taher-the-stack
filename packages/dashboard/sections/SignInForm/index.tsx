bd381676a050fde7cb3a5e5c47feb9b157fad334
Developer <fahimfaisal.soikot@gmail.com>
2022-12-21 18:12:16 +0600

    chore: include tailwind setup
    Can't setup perfectly. some classes prefix focus, peer can not work
    perfectly.

--- packages/dashboard/sections/SignInForm/index.tsx
+++ packages/dashboard/sections/SignInForm/index.tsx
@@ -1,19 +1,65 @@
 // form
+
+// @mui
+import { Stack } from '@mui/material';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 import FormProvider from 'packages/dashboard/components/hook-form/FormProvider';
 import RHFCheckbox from 'packages/dashboard/components/hook-form/RHFCheckBox';
+import RHFPasswordField from 'packages/dashboard/components/hook-form/RHFPasswordField';
 import RHFTextField from 'packages/dashboard/components/hook-form/RHFTextFiled';
-// @mui
-import { LoadingButton } from '@mui/lab';
-import {
-  Link,
-  Stack
-} from '@mui/material';
 // form
 import { useForm } from 'react-hook-form';
+import { twMerge } from 'tailwind-merge';
 
-import axios from 'axios';
-import RHFPasswordField from 'packages/dashboard/components/hook-form/RHFPasswordField';
-import { useRouter } from 'next/router';
+const Button = ({
+  text,
+  clickHandler,
+  classes = '',
+  icon = null,
+  variant = 'blue',
+  ...props
+}) => {
+  const variants = {
+    blue: 'text-blue-600 hover:text-blue-900 hover:border-blue-900 ring-blue-100',
+    red: 'text-red-600 hover:text-red-900 hover:border-red-900 ring-red-100',
+    green:
+      'text-green-600 hover:text-green-900 hover:border-green-900 ring-green-100'
+  };
+  return (
+    <button
+      type="button"
+      onClick={clickHandler}
+      className={twMerge(
+        `px-2 py-1 inline-block rounded border text-sm font-medium text-black hover:bg-transparent focus:outline-none focus:ring ${
+          variants[variant] ?? ''
+        } ${classes}`.trim()
+      )}
+      {...props}
+    >
+      {icon}
+      {text}
+    </button>
+  );
+};
+
+const Input = () => (
+  <label
+    htmlFor="UserEmail"
+    className="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+  >
+    <input
+      type="email"
+      id="UserEmail"
+      placeholder="Email"
+      className="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+    />
+
+    <span className="absolute left-3 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs">
+      Email
+    </span>
+  </label>
+);
 
 interface SignInFormTypes {
   identifier: string;
@@ -85,22 +131,22 @@ const SignInForm = () => {
           name="remember"
           label="Remember me"
         />
-        <Link
-          variant="subtitle2"
-          underline="hover"
-        >
-          Forgot password?
-        </Link>
       </Stack>
 
-      <LoadingButton
+      {/* <LoadingButton
         size="large"
         type="submit"
         variant="contained"
         loading={isSubmitting}
       >
         Sign in
-      </LoadingButton>
+      </LoadingButton> */}
+      <Button
+        text={'Signup'}
+        classes="text-xl"
+        clickHandler={null}
+      />
+      <Input />
     </FormProvider>
   );
 };
