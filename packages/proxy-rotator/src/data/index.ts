b067b4addf8929fc3fe553b42bdea3e16b4c6b53
Fahim Faisaal <57553028+fahimfaisaal@users.noreply.github.com>
2022-12-28 17:18:41 +0600

    Revert "ref: modify rotator functionality through proxy chain"

---
+++ packages/proxy-rotator/src/data/index.ts
@@ -0,0 +1,15 @@
+import {
+  Proxy,
+  ProxyList
+} from '@prisma/client';
+
+export default {
+  proxyList: null,
+  set(
+    data: ProxyList & {
+      Proxies: Proxy[];
+    }
+  ) {
+    this.proxyList = data;
+  }
+};
--- packages/proxy-rotator/src/helpers/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './createCustomError';
-export * from './getProxyList';
-export * from './getRotateProxy';
